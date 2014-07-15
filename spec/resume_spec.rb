2166a3e0d3eaf9c786732002573fb52463955338
Guinsly Mondésir <guinslym@gmail.com>
2014-07-14 22:35:28 -0400

    test the instance of class

--- spec/resume_spec.rb
+++ spec/resume_spec.rb
@@ -1,6 +1,24 @@
 require "spec_helper"
+require_relative "../lib/resume"
+
+require "json"
+class Resume
+  attr_accessor :name, :tel, :email, :experience, :education, :company
+  
+
+end
 
 describe Resume do 
 
+  before :each do
+    @resume = Resume.new 
+  end
+
+  it "#Resume" do
+     expect(@resume).to be_an_instance_of(Resume)
+  end
+  it ".load_file" do
+
+  end
 
 end
