f604a36e45d03867524b95aa8ee6a659a036f88c
Guinsly Mondésir <guinslym@gmail.com>
2014-07-14 22:43:31 -0400

    testing the attributes

--- spec/resume_spec.rb
+++ spec/resume_spec.rb
@@ -5,6 +5,9 @@ require "json"
 class Resume
   attr_accessor :name, :tel, :email, :experience, :education, :company
   
+  def initialize
+  #  file = File.read("resume.json")
+  end
 
 end
 
@@ -16,6 +19,15 @@ describe Resume do
 
   it "#Resume" do
      expect(@resume).to be_an_instance_of(Resume)
+  end
+  
+  it "response to attribute" do
+    expect(@resume).to respond_to(:name)
+    expect(@resume).to respond_to(:tel)
+    expect(@resume).to respond_to(:email)
+    expect(@resume).to respond_to(:experience)
+    expect(@resume).to respond_to(:education)
+    expect(@resume).to respond_to(:company)
   end
   it ".load_file" do
 
