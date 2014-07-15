55165a3f1f3f128228955efd978a3aeed1ce9174
Guinsly Mondésir <guinslym@gmail.com>
2014-07-15 09:03:22 -0400

    books

--- cv-fr.rb
+++ cv-fr.rb
@@ -8,7 +8,7 @@ paragraph "J'ai été initié à Ruby en Novembre 2013, j'étais à Vancouver et
 
 paragraph "Après deux semaines de Ruby je ne comprenais plus rien de mon code php. Je trouvais que Ruby étais vraiment beaucoup plus élégant comme langage. J'ai été au Ruby meetup de Vancouver pour en apprendre d'avantage sur Ruby on Rails"
 
-paragraph "J'ai lu des livres sur Ruby on Rails -- Pragmatic Ruby on Rails;  Beginning Ruby on Rails 4 et Beginning Rails 3, Instant Test development with Rspec Rspec. En décembre je suis parti à Ottawa et j'ai toujours été en contact avec la programmation Ruby; en lisant des livres; en allant aux Meetups d'Ottawa; en suivant un cours de Ruby on Raisl de Casey li"
+paragraph "J'ai lu des livres sur Ruby on Rails -- Pragmatic Ruby on Rails;  Beginning Ruby on Rails 4 et Beginning Rails 3, Agile web development with rails 3. En décembre je suis parti à Ottawa et j'ai toujours été en contact avec la programmation Ruby; en lisant des livres; en allant aux Meetups d'Ottawa; en suivant un cours de Ruby on Raisl de Casey li"
 
 link '@caseyli', 'http://www.caseyli.com/'
 
@@ -19,7 +19,7 @@ link 'mirojune', 'http://mirojune.herokuapp.com/'
 link 'Intergrupal AA-montreal', 'https://alcoholicos-anonimos-montreal.herokuapp.com/'
 link 'amabiblio', 'http://amabiblio.info/'
 
-paragraph "C'est dernier temps je suis entrain de me perfectionner en Ruby et en javascript parce que les projets que j'ai eu jusqu'à présent, je me suis rendu compte que le back-end n'est nécessairement compliqué ou long à faire; ce qui m'a pris le plus de temps c'est le front-end (css3 et javascript)"
+paragraph "C'est dernier temps je suis entrain de me perfectionner en Ruby, en Testing et en javascript parce que les projets que j'ai eu jusqu'à présent, je me suis rendu compte que le back-end n'est nécessairement compliqué ou long à faire; ce qui m'a pris le plus de temps c'est le front-end (css3 et javascript)"
 
 headline "Pourquoi appliquer à Wasappli"
 
