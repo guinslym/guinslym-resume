d09d2faea9268972db64ebd99ce212e224b7d1df
Guinsly Mondésir <guinslym@gmail.com>
2014-07-18 05:55:00 -0400

    add file name

--- lib/resume.rb
+++ lib/resume.rb
@@ -2,43 +2,19 @@ require "json"
 require 'command_line_reporter'
 
 
-
 class Fichier
   attr_accessor :data
+
   def initialize(file = "resume.json")
-  
-  @data = JSON.parse(File.read(file))
-  #puts data = data['bio']['firstName']
+  begin
+    @data = JSON.parse(File.read(file))
+  rescue Exception => e  
+    raise e, "file not, found"
+  end#begin
+
   end
 
   def get_data
     @data
   end
 end
-
-class Resume
-  include CommandLineReporter
-  attr_accessor :name, :tel, :email, :experience, :education
-  
-  def initialize(data)
-  	self.formatter = 'progress'
-    bio = data['bio']
-    @name = bio['firstName'] + ' ' + bio['lastName']
-    @tel = bio['phone']
-    @email = bio['email']
-    @experience = data['work']
-    @education = data['education']
-  end
-
-end
-
-=begin dans le fichier resume.json data contient ces champs
-bio
-work
-education
-awards
-publications
-skills
-references
-=end
-
--- spec/resume_spec.rb
+++ spec/resume_spec.rb
@@ -32,46 +32,14 @@ describe Fichier do
     it "should not raise error with the right file name" do
       expect { file }.to_not raise_error
     end
-    it "should raise an error if file name is not exist" do
+    it "should raise an error if the file couldn't be found" do
       expect { bad_name }.to raise_error ("file not, found")
     end#it raise error
   end#context
   context "File Content" do
-    let(:file) { Fichier.new("resume.json")}
+    let(:file) { Fichier.new("resume.json").get_data }
     it "should not be nil " do
       expect(file).not_to be_nil
     end
   end
 end#describe Fichier
-
-
-=begin
-describe Resume do 
-
-  before :each do
-    file = File.read("./resume.json")
-    data = JSON.parse(file)
-    @resume = Resume.new(data)
-  end
-
-  it "#Resume" do
-     expect(@resume).to be_an_instance_of(Resume)
-  end
-  
-  it "should respond to its attributes" do
-    expect(@resume).to respond_to(:name)
-    expect(@resume).to respond_to(:tel)
-    expect(@resume).to respond_to(:email)
-    expect(@resume).to respond_to(:experience)
-    expect(@resume).to respond_to(:education)
-  end
-  it "attributes should not be nil" do
-    expect(@resume.name).not_to be_nil
-    expect(@resume.tel.size).not_to be_nil
-    expect(@resume.experience.size).not_to be_nil
-    expect(@resume.email.size).not_to be_nil
-    expect(@resume.education.size).not_to be_nil
-  end
-
-end
-=end
