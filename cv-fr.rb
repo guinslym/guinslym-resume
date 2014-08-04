eaf73bb4a81ab78ee3e2cfdc2bcf6b3b795e254f
Guinsly Mondésir <guinslym@gmail.com>
2014-08-03 21:20:58 -0400

    standardiser mon curriculum

--- cv-fr.rb
+++ cv-fr.rb
@@ -8,22 +8,21 @@ paragraph "J'ai été initié à Ruby en Novembre 2013, j'étais à Vancouver et
 
 paragraph "Après deux semaines de Ruby je ne comprenais plus rien de mon code php. Je trouvais que Ruby étais vraiment beaucoup plus élégant comme langage. J'ai été au Ruby meetup de Vancouver pour en apprendre d'avantage sur Ruby on Rails"
 
-paragraph "J'ai lu des livres sur Ruby on Rails -- Pragmatic Ruby on Rails;  Beginning Ruby on Rails 4 et Beginning Rails 3, Agile web development with rails 3. En décembre je suis parti à Ottawa et j'ai toujours été en contact avec la programmation Ruby; en lisant des livres; en allant aux Meetups d'Ottawa; en suivant un cours de Ruby on Raisl de Casey li"
+paragraph "J'ai lu des livres sur Ruby on Rails -- Pragmatic Ruby on Rails;  Beginning Ruby on Rails 4 et Beginning Rails 3, Agile web development with rails 3. Teting with Rspec. En décembre je suis parti à Ottawa et j'ai toujours été en contact avec la programmation Ruby; en lisant des livres; en allant aux Meetups d'Ottawa; en suivant un cours de Ruby on Raisl de Casey li"
 
 link '@caseyli', 'http://www.caseyli.com/'
 
 paragraph "J'ai fait des projets de bénévolats pour mieux maîtriser Ruby on Rails. Parce que pour appliquer dans les startups ils demaindent souvent si j'ai de l'expérience Réel en Ruby on Rails"
 
-link 'grupo-azteca', 'http://grupo-azteca-montreal.herokuapp.com/'
 link 'mirojune', 'http://mirojune.herokuapp.com/'
 link 'Intergrupal AA-montreal', 'https://alcoholicos-anonimos-montreal.herokuapp.com/'
 link 'amabiblio', 'http://amabiblio.info/'
 
 paragraph "C'est dernier temps je suis entrain de me perfectionner en Ruby, en Testing et en javascript parce que les projets que j'ai eu jusqu'à présent, je me suis rendu compte que le back-end n'est nécessairement compliqué ou long à faire; ce qui m'a pris le plus de temps c'est le front-end (css3 et javascript)"
 
-headline "Pourquoi appliquer à Wasappli"
+headline "Pourquoi m'engageriez vous dans votre compagnie"
 
-paragraph "J'ai été attiré par le fait que vous faîtes des applications mobiles; Quand j'étais à Vancouver tout le monde que j'approchais faisaient des applications mobiles (phonegap; rubymotion; lua; scala; IOS/android native) et ce n'est pas quelque choses que j'ai appris sur les bancs d'école; sauf exception android"
+paragraph "J'ai été attiré par le fait que vous êtes interessé à avoir comme developpeur quelqu'un qui se soucie du UI/UX de votre application ainsi que la scalabilité de votre application. Je suis aussi quelqu'un qui saura mettre du piment dans votre équipe pour augmenter la créativité dans les sessions de Brainstorming"
 
 headline "C'est le temps de poser des questions"
 
